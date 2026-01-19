76defa24ca2a16b9a18dec27f81105a76d67a6a6
Soumya_jain <jaxiver377@gmail.com>
2026-01-19 23:16:40 +0530

    Time: 45 ms (71.69%) | Memory: 56.5 MB (43.79%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {any[]} arr
+ * @param {number} size
+ * @return {any[][]}
+ */
+var chunk = function(arr, size) {
+    let res = [];
+    
+    for (let i = 0; i < arr.length; i += size) {
+        res.push(arr.slice(i, i + size));
+    }
+    
+    return res;
+};
